8402f49a97a66540c1c305295380b137851c02b2
gunnarro <gunnar.ronneberg@gmail.com>
2014-04-06 16:26:39 +0200

    code update

---
+++ src/com/gunnarro/android/bandy/domain/League.java
@@ -0,0 +1,25 @@
+package com.gunnarro.android.bandy.domain;
+
+import java.io.Serializable;
+
+public class League implements Serializable {
+
+	private static final long serialVersionUID = 243232L;
+
+	private Integer id;
+	private String name;
+
+	public League(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ src/com/gunnarro/android/bandy/repository/table/link/ContactRoleTypeLnkTable.java
@@ -0,0 +1,37 @@
+package com.gunnarro.android.bandy.repository.table.link;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.gunnarro.android.bandy.repository.table.TableHelper;
+import com.gunnarro.android.bandy.repository.table.party.RoleTypesTable;
+
+public class ContactRoleTypeLnkTable {
+
+	// Database table
+	public static final String TABLE_NAME = "contact_role_type_lnk";
+	public static final String COLUMN_FK_CONTACT_ID = "fk_contact_id";
+	public static final String COLUMN_FK_ROLE_TYPE_ID = "fk_role_type_id";
+	public static final String[] TABLE_COLUMNS = { COLUMN_FK_CONTACT_ID, COLUMN_FK_CONTACT_ID };
+
+	public static void onCreate(SQLiteDatabase database) {
+		LinkTableHelper.onCreate(database, TABLE_NAME, COLUMN_FK_CONTACT_ID, COLUMN_FK_ROLE_TYPE_ID, TABLE_NAME, RoleTypesTable.TABLE_NAME);
+	}
+
+	public static void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+		LinkTableHelper.onUpgrade(database, TABLE_NAME, COLUMN_FK_CONTACT_ID, COLUMN_FK_ROLE_TYPE_ID, TABLE_NAME, RoleTypesTable.TABLE_NAME, oldVersion,
+				newVersion);
+	}
+
+	public static String[] getIdTableColumns() {
+		return new String[] { COLUMN_FK_CONTACT_ID, COLUMN_FK_ROLE_TYPE_ID };
+	}
+
+	public static String[] getTableColumns() {
+		return TableHelper.createColumns(getIdTableColumns());
+	}
+
+	public static ContentValues createContentValues(Integer contactId, Integer roleTypeId) {
+		return LinkTableHelper.createContentValues(new String[] { COLUMN_FK_CONTACT_ID, COLUMN_FK_ROLE_TYPE_ID }, new Integer[] { contactId, roleTypeId });
+	}
+}
